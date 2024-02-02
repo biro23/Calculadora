e0940471b563beec20e91d2ced4cd414d01ba5f7
biro23 <121694408+biro23@users.noreply.github.com>
2024-02-02 12:21:06 -0300

    first commit

---
+++ primeiro_projeto_vue/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+const {saldo, transferindo} = estado;
+function MostraSaldoFuturo(){
+  return estado.saldo - transferindo
+}
+</script>
+
+
+
+
+
+<template>
+  <header>
+    Saldo: {{ estado.saldo }} <br/>
+    Transferindo: {{ estado.transferindo }} <br/>
+    Saldo depois da transferência: {{ MostraSaldoFuturo }} <br/>
+   <input type="number" placeholder="Quantia para transferir"/>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
